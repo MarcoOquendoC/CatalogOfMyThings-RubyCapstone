ca8f672b11d3975089774909b46acc72646cae86
Fábio Vinícius Lima <limavfabio@gmail.com>
2023-01-10 10:57:38 -0300

    Add prints for Item class on app.rb

--- app.rb
+++ app.rb
@@ -1,5 +1,16 @@
+require_relative 'src/item'
+
+
 class App
   def run
-    puts 'hello App'
+    item = Item.new
+    puts 'item.publish_date'
+    puts item.publish_date
+    puts 'item.id'
+    puts item.id
+    puts 'item.can_be_archived'
+    puts item.can_be_archived?
+    puts 'item.genre'
+    puts item.genre
   end
 end
