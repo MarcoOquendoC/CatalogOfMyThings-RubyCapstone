442974088b9c52b8c6c69d7afa07f6f17dde8f29
Fábio Vinícius Lima <limavfabio@gmail.com>
2023-01-11 18:40:14 -0300

    Implemented options to add and list books

--- app_modules/book/add_book.rb
+++ app_modules/book/add_book.rb
@@ -1,6 +1,17 @@
 module AddBook
   def add_book
-    puts 'code to process add book'
+    puts 'Enter book publisher'
+    publisher = gets.chomp
+    puts 'Enter book cover state'
+    cover_state = gets.chomp
+    puts 'Enter book publish date'
+    publish_date = gets.chomp
+
+    @books_list << Book.new(publisher, cover_state, publish_date)
+    system('clear')
+    puts 'Book added succesfully!'
+    puts 'Press Enter to return to main menu'
+    gets.chomp
     run
   end
 end
--- app_modules/book/list_books.rb
+++ app_modules/book/list_books.rb
@@ -1,6 +1,15 @@
 module ListBooks
   def list_books
-    puts 'code to process list_books'
+    puts 'List of all books'
+    puts ''
+
+    @books_list.each do |book|
+      puts "Publisher: #{book.publisher}, Cover State: #{book.cover_state}, Publish Date: #{book.publish_date}"
+    end
+
+    puts ''
+    puts 'Press Enter to return to main menu'
+    gets.chomp
     run
   end
 end
