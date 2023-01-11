ddd7912feab2ba3c075e355af273bb03babd404c
Fábio Vinícius Lima <limavfabio@gmail.com>
2023-01-11 12:32:44 -0300

    Create label class

---
+++ book/label.rb
@@ -0,0 +1,18 @@
+require 'securerandom'
+
+class Label
+  attr_accessor :title, :color
+  attr_reader :items, :id
+
+  def initialize(title, color, id = SecureRandom.uuid)
+    @title = title
+    @color = color
+    @id = id
+    @items = []
+  end
+
+  def add_item(item)
+    @items.push(item)
+    item.label = title
+  end
+end
