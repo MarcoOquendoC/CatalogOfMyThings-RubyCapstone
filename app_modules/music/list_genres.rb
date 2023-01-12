3a818ad5402f300985fc584fc724aa62c7bd783f
Marco Oquendo <marcooquendoc@gmail.com>
2023-01-12 16:15:34 -0500

    List genres code

--- app_modules/music/list_genres.rb
+++ app_modules/music/list_genres.rb
@@ -1,6 +1,30 @@
 module ListGenres
   def list_genres
-    puts 'code to process list_genres'
+    greet_genres
+
+    @genres.map do |genre|
+      items = genre[:items].map {|item| "#{item.class.name} - #{item.name}"}
+      puts '{'
+      puts "  ID: #{genre[:id]}"
+      puts "  Name: #{genre[:name]}"
+      puts "  Items: #{ items }"
+      puts '}'
+    end
+
+    puts 'press enter to continue'
+    gets
+    system('clear')
+    system('cls')
     run
+  end
+
+  def greet_genres
+    # I duplicated backslashes \ to print them
+    puts ".____    .__          __      ________                                    "
+    puts "|    |   |__| _______/  |_   /  _____/  ____   ___________   ____   ______"
+    puts "|    |   |  |/  ___/\\   __\\ /   \\  ____/ __ \\ /    \\_  __ \\_/ __ \\ /  ___/ "
+    puts "|    |___|  |\\___ \\  |  |   \\    \\_\\  \\  ___/|   |  \\  | \\/\\  ___/ \\___ \\ "
+    puts "|_______ \\__/____  > |__|    \\______  /\\___  >___|  /__|    \\___  >____  >"
+    puts "        \\/       \\/                 \\/     \\/     \\/            \\/     \\/ "
   end
 end
