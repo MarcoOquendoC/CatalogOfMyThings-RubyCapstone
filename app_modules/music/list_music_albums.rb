299700cc60df1f54fd190e9e533a47d909be9d27
Marco Oquendo <marcooquendoc@gmail.com>
2023-01-12 16:15:56 -0500

    List albums code

--- app_modules/music/list_music_albums.rb
+++ app_modules/music/list_music_albums.rb
@@ -1,6 +1,32 @@
 module ListMusicAlbums
   def list_music_albums
-    puts 'code to process list_albums'
+    greet_albums
+
+    @music_albums.map do |album|
+      puts '{'
+      puts "  Name: #{album[:name]}"
+      puts "  Artist: #{album[:artist]}"
+      puts "  Genre: #{album[:genre][:name]}"
+      puts "  On Spotify?: #{album[:on_spotify]}"
+      puts "  Archived: #{album[:archived]}"
+      puts "  Publish Date: #{album[:publish_date]}"
+      puts '}'
+    end
+
+    puts 'press enter to continue'
+    gets
+    system('clear')
+    system('cls')
     run
+  end
+
+  def greet_albums
+    # I duplicated backslashes \ to print them
+    puts ".____    .__          __       _____  .__ ___.                         "
+    puts "|    |   |__| _______/  |_    /  _  \\ |  |\\_ |__  __ __  _____   ______"
+    puts "|    |   |  |/  ___/\\   __\\  /  /_\\  \\|  | | __ \\|  |  \\/     \\ /  ___/"
+    puts "|    |___|  |\\___ \\  |  |   /    |    \\  |_| \\_\\ \\  |  /  Y Y  \\___ \\ "
+    puts "|_______ \\__/____  > |__|   \\____|__  /____/___  /____/|__|_|  /____  >"
+    puts "        \\/       \\/                 \\/         \\/            \\/     \\/ "
   end
 end
