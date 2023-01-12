d1685f0dffb0c5fd03113c7b6599a83ffa55ff37
Fábio Vinícius Lima <limavfabio@gmail.com>
2023-01-12 16:46:28 -0300

    Add logic to add label to book on creation

--- app_modules/book/add_book.rb
+++ app_modules/book/add_book.rb
@@ -6,8 +6,17 @@ module AddBook
     cover_state = gets.chomp
     puts 'Enter book publish date'
     publish_date = gets.chomp
+    puts 'Enter label title for the book'
+    label_title = gets.chomp
+    puts 'Enter label color'
+    label_color = gets.chomp
 
-    @books_list << Book.new(publisher, cover_state, publish_date)
+    new_book = Book.new(publisher, cover_state, publish_date)
+    new_label = Label.new(label_title, label_color)
+    new_label.add_item(new_book)
+    @labels_list << new_label
+    @books_list << new_book
+
     system('clear')
     puts 'Book added succesfully!'
     puts 'Press Enter to return to main menu'
