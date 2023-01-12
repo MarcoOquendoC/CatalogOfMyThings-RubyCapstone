c9ef17e8816524c47b98705ffb8b8f89eb4a0d1f
Marco Oquendo <marcooquendoc@gmail.com>
2023-01-12 16:15:15 -0500

    Add Greetings and goodbye

---
+++ app_modules/goodbye.rb
@@ -0,0 +1,14 @@
+module Goodbye
+  # Generated from https://edukits.co/text-art/ (duplicate \ to print them)
+  def goodbye
+    puts ".-. .-')                 ('-.  ,---. "
+    puts "\\  ( OO )              _(  OO) |   | "
+    puts " ;-----.\\  ,--.   ,--.(,------.|   | "
+    puts " | .-.  |   \\  `.'  /  |  .---'|   | "
+    puts " | '-' /_).-')     /   |  |    |   | "
+    puts " | .-. `.(OO  \\   /   (|  '--. |  .' "
+    puts " | |  \\  ||   /  /\\_   |  .--' `--'  "
+    puts " | '--'  /`-./  /.__)  |  `---..--.  "
+    puts " `------'   `--'       `------''--' "
+  end
+end
