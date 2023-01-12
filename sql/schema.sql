bfe91b26aa74e1e5a3b9db497be10fb8cf293ee1
Miliyonayalew <miliayalew@gmail.com>
2023-01-13 00:07:27 +0300

    Create games table

--- sql/schema.sql
+++ sql/schema.sql
@@ -8,3 +8,16 @@ CREATE TABLE authors{
   last_name VARCHAR(100),
   PRIMARY KEY(id)
 }
+
+-- game table
+
+CREATE TABLE games{
+  id INT GENERATED ALWAYS AS IDENTITY,
+  publish_date DATE,
+  archived BOOLEAN,
+  multiplayer BOOLEAN,
+  last_played_at DATE,
+  game_id INT
+  PRIMARY KEY(id)
+  CONSTRAINT fk_author FOREIGN KEY(games_id) REFERENCES authors(id)
+}
