8a479df675d13a9c3886479b980e0cbc2fd9763d
Marco Oquendo <marcooquendoc@gmail.com>
2023-01-12 21:00:01 -0500

    List albums feature UI

--- app_modules/music/list_music_albums.rb
+++ app_modules/music/list_music_albums.rb
@@ -3,13 +3,17 @@ module ListMusicAlbums
     greet_albums
 
     @music_albums.map do |album|
+      genre = [
+                "ID: #{album.genre.id}",
+                "Name: #{album.genre.name}",
+                "Item(s): #{album.genre.items.count}"
+              ]
       puts '{'
-      puts "  Name: #{album[:name]}"
-      puts "  Artist: #{album[:artist]}"
-      puts "  Genre: #{album[:genre][:name]}"
-      puts "  On Spotify?: #{album[:on_spotify]}"
-      puts "  Archived: #{album[:archived]}"
-      puts "  Publish Date: #{album[:publish_date]}"
+      puts "  Name: #{album.name}"
+      puts "  On Spotify?: #{album.on_spotify}"
+      puts "  Archived: #{album.archived}"
+      puts "  Publish Date: #{album.publish_date}"
+      puts "  Genre: #{genre}"
       puts '}'
     end
 
@@ -21,12 +25,12 @@ module ListMusicAlbums
   end
 
   def greet_albums
-    # I duplicated backslashes \ to print them
-    puts ".____    .__          __       _____  .__ ___.                         "
-    puts "|    |   |__| _______/  |_    /  _  \\ |  |\\_ |__  __ __  _____   ______"
-    puts "|    |   |  |/  ___/\\   __\\  /  /_\\  \\|  | | __ \\|  |  \\/     \\ /  ___/"
-    puts "|    |___|  |\\___ \\  |  |   /    |    \\  |_| \\_\\ \\  |  /  Y Y  \\___ \\ "
-    puts "|_______ \\__/____  > |__|   \\____|__  /____/___  /____/|__|_|  /____  >"
-    puts "        \\/       \\/                 \\/         \\/            \\/     \\/ "
+    # Generated from https://edukits.co/text-art/ (duplicate \ to print them)
+    puts '.____    .__          __       _____  .__ ___.                         '
+    puts '|    |   |__| _______/  |_    /  _  \\ |  |\\_ |__  __ __  _____   ______'
+    puts '|    |   |  |/  ___/\\   __\\  /  /_\\  \\|  | | __ \\|  |  \\/     \\ /  ___/'
+    puts '|    |___|  |\\___ \\  |  |   /    |    \\  |_| \\_\\ \\  |  /  Y Y  \\___ \\ '
+    puts '|_______ \\__/____  > |__|   \\____|__  /____/___  /____/|__|_|  /____  >'
+    puts '        \\/       \\/                 \\/         \\/            \\/     \\/ '
   end
 end
