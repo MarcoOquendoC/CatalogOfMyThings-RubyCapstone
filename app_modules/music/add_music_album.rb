d7393984d7daed9b6948a890e1d4b538943ac09c
Marco Oquendo <marcooquendoc@gmail.com>
2023-01-12 21:01:11 -0500

    Add new music album functionality

--- app_modules/music/add_music_album.rb
+++ app_modules/music/add_music_album.rb
@@ -1,6 +1,48 @@
 module AddMusicAlbum
   def add_music_album
-    puts 'code to process add music album'
+    greet_add_album
+
+    print 'Enter the music album\'s genre: '
+    genre_name = gets.chomp
+
+    print 'Enter the music album\'s name: '
+    album_name = gets.chomp
+
+    print "\nWhat's the publish date? [year]: "
+    album_date = gets.chomp.to_i
+
+    print 'Is this Music Album on Spotify? [y/n]: '
+    spotify = gets.chomp
+    is_on_spotify = case spotify.downcase
+                    when 'y'
+                      true
+                    else
+                      false
+                    end
+
+    music_album = MusicAlbum.new(album_date, album_name, on_spotify: is_on_spotify)
+
+    new_genre = Genre.new(genre_name)
+    new_genre.add_item(music_album)
+
+    @genres << new_genre
+    @music_albums << music_album
+
+    puts 'Album added successfully!'
+    puts 'Press enter to continue'
+    gets
+    system('clear')
+    system('cls')
     run
+  end
+
+  def greet_add_album
+    # Generated from https://edukits.co/text-art/ (duplicate \ to print them)
+    puts '               _____  .__ ___.                  '
+    puts '    .__       /  _  \\ |  |\\_ |__  __ __  _____  '
+    puts '  __|  |___  /  /_\\  \\|  | | __ \\|  |  \\/     \\ '
+    puts ' /__    __/ /    |    \\  |_| \\_\\ \\  |  /  Y Y  \\'
+    puts '    |__|    \\____|__  /____/___  /____/|__|_|  /'
+    puts '                    \\/         \\/            \\/ '
   end
 end
